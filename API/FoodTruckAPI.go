8c5c038d65d40dfe6ff117efa71a924881bafa9b
Mark <mnkolev@gmail.com>
2017-07-11 23:46:48 -0400

    small changes

--- API/FoodTruckAPI.go
+++ API/FoodTruckAPI.go
@@ -16,3 +16,39 @@ func main() {
     log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
+
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+//     "net/http"
+
+//     "github.com/gorilla/mux"
+// )
+
+// func main() {
+
+//     router := mux.NewRouter().StrictSlash(true)
+//     router.HandleFunc("/", Index)
+//     router.HandleFunc("/todos", TodoIndex)
+//     router.HandleFunc("/todos/{todoId}", TodoShow)
+
+//     fmt.Println("Listening on port 8080")
+
+//     log.Fatal(http.ListenAndServe(":8080", router))
+// }
+
+// func Index(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintln(w, "Welcome!")
+// }
+
+// func TodoIndex(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintln(w, "Todo Index!")
+// }
+
+// func TodoShow(w http.ResponseWriter, r *http.Request) {
+//     vars := mux.Vars(r)
+//     todoId := vars["todoId"]
+//     fmt.Fprintln(w, "Todo show:", todoId)
+// }
